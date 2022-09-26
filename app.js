cc6edb667b1e1d335e8ad96b1578b69ff0ec52c0
NIKET KUMAR MISHRA <niketvns97@gmail.com>
2022-09-26 11:54:14 +0530

    docs: all files added

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 var declare = document.querySelector("#declare");
 var button = document.querySelector(".button");
+var quizForm = document.querySelector(".quiz-form");
+var score_html = document.querySelector("#score");
+var resultHeader = document.querySelector(".result-header")
+var quizHeader = document.querySelector(".quiz-header")
+
+
+const correctAnswer = [
+    "90",
+    "right angle",
+    "one right angle",
+    "12,16,20",
+    "equilateral triangle",
+    "100",
+    "30",
+    "a+b+c",
+    "no",
+    "45",
+    "not question"
+]
 
 declare.addEventListener("click", function () {
     if (declare.checked == true) {
@@ -12,3 +31,25 @@ declare.addEventListener("click", function () {
         button.style.display = "none";
     }
 })
+
+
+button.addEventListener("click", calculateScore);
+
+function calculateScore() {
+    const formData = new FormData(quizForm);
+    let score = 0;
+    let index = 0;
+    for (let value of formData.values()) {
+        for (let check = 0; check < 10; check++) {
+            if (correctAnswer[check] === value) {
+                score = score + 1;
+            }
+        }
+        index = index + 1;
+    }
+
+    resultHeader.style.display = "contents";
+    quizHeader.style.display = "none";
+
+    score_html.innerHTML = "Your Score: " + score + "/10";
+}
