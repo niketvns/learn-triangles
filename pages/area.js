78a63b146cb3adf62fad78fc164afa37d588dfa2
NIKET KUMAR MISHRA <niketvns97@gmail.com>
2022-09-27 00:14:38 +0530

    fix: heron's formula sides

--- pages/area.js
+++ pages/area.js
@@ -3,15 +3,22 @@ var side2 = document.querySelector("#side2");
 var side3 = document.querySelector("#side3");
 var button = document.querySelector("button");
 var areaOutput = document.querySelector(".areaOutput");
+var errorOutput = document.querySelector(".errorOutput");
 
 button.addEventListener("click", calculateArea)
 
 
 function calculateArea() {
     var sp = (side1.value * 1 + side2.value * 1 + side3.value * 1) / 2;
-    // alert(sp)
     var squreArea = sp * (sp - side1.value * 1) * (sp - side2.value * 1) * (sp - side3.value * 1);
-    var area = Math.sqrt(squreArea)
-    areaOutput.innerHTML = "Area: " + area.toFixed(2)
-    areaOutput.style.color = "Green";
+
+    if (squreArea < 0) {
+        areaOutput.innerHTML = ""
+        errorOutput.innerHTML = "The sum of two sides must be larger than the third. \n The Value You have entered can not make a triangle";
+        errorOutput.style.color = "Red";
+    } else {
+        var area = Math.sqrt(squreArea)
+        areaOutput.innerHTML = "Area: " + area.toFixed(2)
+        areaOutput.style.color = "Green";
+    }
 }
